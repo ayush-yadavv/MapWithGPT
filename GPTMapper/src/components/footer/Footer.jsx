a3deb757e5d284e6b50d59c282a31b63a3fdd8c4
ayush <yadav.ayushx1@gmail.com>
2024-01-25 01:05:02 +0530

    cc

--- GPTMapper/src/components/footer/Footer.jsx
+++ GPTMapper/src/components/footer/Footer.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div className='footer-area'>
-          
-        <p>Copyright ©2023 All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i>  by    <span className='teamname'> MicroCoders</span></p>
-        
-        
-      
+    <div className="footer-area">
+      <p>
+        Copyright ©2024 All rights reserved | This template is made with{" "}
+        <i class="fa fa-heart-o" aria-hidden="true"></i> by{" "}
+        <span className="teamname"> MicroCoders</span>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
